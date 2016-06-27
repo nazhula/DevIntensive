eb2f76e2c657878d5e36d1a63376780a65baa8c4
nazhula <nazhula@example.com>
2016-06-27 22:53:52 +0400

    NavigationDrawer (draft)

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.app.ActionBar;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.ui.utils.ConstantManager;
@@ -12,13 +16,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private static final String TAG= ConstantManager.TAG_PREFIX + "Main Activity";
-
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+
+        setupToolbar();
 
         if (savedInstanceState == null) {
             //активити запускается впервые
@@ -73,6 +83,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState");
+    }
+
+    private void setupToolbar(){
+     setSupportActionBar(mToolBar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
 
