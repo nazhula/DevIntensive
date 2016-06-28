d8a3bb956d9ca65ba92599b44fdcd581991e1758
nazhula <nazhula@example.com>
2016-06-28 20:15:17 +0400

    NavigationDrawer + close  button "Back"

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,23 +1,29 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.app.ActionBar;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.ui.utils.ConstantManager;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    private static final String TAG= ConstantManager.TAG_PREFIX + "Main Activity";
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
     private CoordinatorLayout mCoordinatorLayout;
     private Toolbar mToolBar;
+    private ImageView mUserAvatar;
+    private DrawerLayout mNavigationDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
         mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        mUserAvatar = (ImageView) findViewById(R.id.user_avatar);
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
 
         setupToolbar();
+        //setupDrawer();
 
         if (savedInstanceState == null) {
             //активити запускается впервые
@@ -85,14 +94,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.d(TAG, "onSaveInstanceState");
     }
 
-    private void setupToolbar(){
-     setSupportActionBar(mToolBar);
+    private void setupToolbar() {
+        setSupportActionBar(mToolBar);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
+
+    //по кнопке back закрываем Navigation Drawer
+    @Override
+    public void onBackPressed() {
+        if (mNavigationDrawer.isDrawerOpen(Gravity.LEFT)) {
+            mNavigationDrawer.closeDrawer(GravityCompat.START);
+            return;
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
 
 
